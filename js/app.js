d22a1c1d55d247966b6f665e9a92ff917b9cc796
Sergio <sergiodebei84@gmail.com>
2014-02-08 02:43:31 +0100

    Set-up iniziale: Bootstrap LeafLet e librerie di base
    Signed-off-by: Sergio <sergiodebei84@gmail.com>

---
+++ js/app.js
@@ -0,0 +1,14 @@
+(function (w, L) {
+
+	var map = L.map("map");
+
+	// create the tile layer with correct attribution
+	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osmAttrib='Map data © OpenStreetMap contributors';
+	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});		
+
+	// start the map in South-East England
+	map.setView(new L.LatLng(51.3, 0.7),9);
+	map.addLayer(osm);
+
+})(window, window.L);
