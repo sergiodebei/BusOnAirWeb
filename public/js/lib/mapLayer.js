1809abd5ced7173ab917525a1cea4c7a9cab1627
frenz <giuseppe@gtgames.it>
2014-05-24 13:40:24 +0200

    update more features

--- public/js/lib/mapLayer.js
+++ public/js/lib/mapLayer.js
@@ -8,77 +8,174 @@ var map = L.map("map", {zoomControl: false});
 // create the tile layer with correct attribution
 var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var osmAttrib='Map data © OpenStreetMap contributors';
-var osm = new L.TileLayer(osmUrl, {minZoom: 14, maxZoom: 20, attribution: osmAttrib});	
+var osm = new L.TileLayer(osmUrl, {minZoom: 12, maxZoom: 20, attribution: osmAttrib});	
 
-// Extend the Default marker class
-
+// Extend the Default marker class - Red icon
 var RedIcon = L.Icon.Default.extend({
-
 	options: {
-
-		iconUrl: 'https://maps.gstatic.com/mapfiles/ms2/micons/red-dot.png' 
-
+		iconUrl: 'img/redMarker.png',
+		iconSize:     [41, 41], // size of the icon
 	}
-
 });
-
 var redIcon = new RedIcon();
 
+// Extend the Default marker class - Bus stop icon
+var busStop = L.Icon.Default.extend({
+	options: {
+		iconUrl: 'img/busStop.png',
+		// iconSize:     [21, 75], // size of the icon
+	}
+});
+var busStop = new busStop();
+
+// Extend the Default marker class - Start icon
+var startIcon = L.Icon.Default.extend({
+	options: {
+		iconUrl: 'img/startIcon.png',
+	}
+});
+var startIcon = new startIcon();
+
+// Extend the Default marker class - End icon
+var endIcon = L.Icon.Default.extend({
+	options: {
+		iconUrl: 'img/endIcon.png',
+	}
+});
+var endIcon = new endIcon();
 
 
-
-// start the map in South-East England
+// start the map in l'aquila
 map.locate({setView: true, minZoom: 14, maxZoom: 16});
+
+
+// creare un marker
+
+
+
+
 
 function onLocationFound(e) {
     var radius = 150;
-
     var markers = [];
-
     layer=undefined;
     L.marker(e.latlng,{icon:redIcon,draggable:true})
-
 		.addTo(map)
-
-		.bindPopup("You are within " + radius + " meters from this point").openPopup()
-
+		.bindPopup("You are here").openPopup()
 		.on('dragend', function(event) {
-
 		    if(typeof layer !== 'undefined') map.removeLayer(layer);
 		    var marker = event.target;  // you could also simply access the marker through the closure
-
 		    var result = marker.getLatLng();  // but using the passed event is cleaner
-
-		    $.getJSON( baseURL + "/stations/getneareststations?lat=" + result.lat + "&lon=" + result.lng + "&range=300", function( data ) {
-
+		    $.getJSON( baseURL + "/stations/getneareststations?lat=" + result.lat + "&lon=" + result.lng + "&range=500", function( data ) {
 				$.each( data.stationlist, function( key, val ) {
-
 					$.getJSON( baseURL + val, function( datata ) {
-
-							markers [key] = L.marker([datata.latLon.lat, datata.latLon.lon],{title : datata.name, alt : datata.url });
-
+							markers [key] = L.marker([datata.latLon.lat, datata.latLon.lon],{
+								icon:busStop, title : datata.name, alt : datata.url });
 					});
-
 				});
 				setTimeout(function(){
 					layer = L.layerGroup(markers).addTo(map);
 				},300)
-			
-
 			});
-
-				
-
-
-
 		});
 
-
-    //L.circle(e.latlng, radius).addTo(map);
-
 }
-
 map.on('locationfound', onLocationFound);
+
+
+
+layerTrip=undefined;
+markersTrip = [];
+//mostra la partenza 
+fromto = function() {
+	var that = this;
+	var URL = 'http://nominatim.openstreetmap.org/search?q=';
+   	var val = this.value.replace(" ","%20");
+   	var citta = ',+laquila';
+  	var format = 'json';
+    $.getJSON(URL + val + citta + "&format=" + format, function( nomi ) {
+    	if(typeof layerTrip !== 'undefined') map.removeLayer(layerTrip);	
+		var via_lat = (nomi[0].lat);
+		var via_lon = (nomi[0].lon);
+		if (that.id == 'nominatim1') {
+			markersTrip[0] = L.marker([via_lat, via_lon],{icon:startIcon,draggable:true});
+
+			markersTrip[0].on('dragend', function(event) {
+			    var marker = event.target;  // you could also simply access the marker through the closure
+			    start = marker.getLatLng();  // but using the passed event is cleaner
+			    // console.log(start.lat);
+			    // console.log(start.lng);
+				var URL = 'http://nominatim.openstreetmap.org/reverse?';
+			  	var format = 'json';
+	   			$.getJSON(URL + "&format=" + format + "&lat=" + start.lat + "&lon=" + start.lng, function( reverse ){
+	   				nominatim1.value = reverse.address.road;
+	   			});
+			});
+		} 
+		if(that.id == 'nominatim2'){
+			markersTrip[1] = L.marker([via_lat, via_lon],{icon:endIcon,draggable:true});
+
+					markersTrip[1].on('dragend', function(event) {
+				    var marker = event.target;  // you could also simply access the marker through the closure
+				    end = marker.getLatLng();  // but using the passed event is cleaner
+				    // console.log(end.lat);
+				    // console.log(end.lng);
+					var URL = 'http://nominatim.openstreetmap.org/reverse?';
+				  	var format = 'json';
+		   			$.getJSON(URL + "&format=" + format + "&lat=" + end.lat + "&lon=" + end.lng, function( reverse ){
+			   			nominatim2.value = reverse.address.road;
+			   		});
+					});
+
+
+			};
+		layerTrip = L.layerGroup(markersTrip).addTo(map);
+		// layerTrip.addTo(map);
+	});
+};
+
+
+
+stopLayer=undefined;
+line=undefined;
+showRoute = function(data) {
+
+	if(typeof stopLayer !== 'undefined') map.removeLayer(stopLayer);
+	if(typeof line !== 'undefined') map.removeLayer(line);
+
+	var markers = [];
+	// 			$.each( data.routelist, function( key, val ) {
+	// 							markers [key] = L.marker([data.routelist[key].latLon.lat, data.routelist[key].latLon.lon],{
+	// 							icon:busStop, title : data.routelist[key].stopId, alt : data.routelist[key].stopName });
+	// 				});
+	
+
+var marker = data.routelist;
+var linePts = [];
+	for (var it in marker) {
+		// console.log([data.routelist[it].latLon.lat,data.routelist[it].latLon.lon]);
+		linePts[it] = [data.routelist[it].latLon.lat,data.routelist[it].latLon.lon];
+	}
+
+	for( i=0; i < linePts.length; i=i+1 ) {
+	    // turn this coordinate into a LatLng
+	  linePts[i] = new L.LatLng( linePts[ i ][ 0 ], linePts[ i ][ 1 ] );
+	 		markers [i] = L.marker([linePts[ i ].lat, linePts[ i ].lng],{
+			icon:busStop});
+	}
+
+	// add the line
+	line = new L.Polyline( linePts, { 
+						color: 'rgb(31, 177, 154)',
+	               		weight: 5,
+	                	opacity: .7
+									} );
+	map.addLayer(line);
+	stopLayer = L.layerGroup(markers).addTo(map);
+	// zoom the map to the polyline
+	map.fitBounds(line.getBounds());
+}
+	
 
 map.addLayer(osm);
 
@@ -87,6 +184,9 @@ map.addControl( L.control.zoom({position: 'topright'}) );
 
 // map.addControl( L.myIcon('icon', {position: 'topright'}) );
 
+
+
+// create the logo BusOnAir on the right
 var logo = L.control({position: 'bottomright'});
 
 logo.onAdd = function (map) {
@@ -96,7 +196,6 @@ var div = L.DomUtil.create('div', 'info legend');
 return div;
 };
 
-// Add this one (only) for now, as the Population layer is on by default
 logo.addTo(map);
 
 
--- public/js/lib/mystyle.js
+++ public/js/lib/mystyle.js
@@ -1,5 +1,19 @@
 
 $(function	()	{
+	var baseURL = 'http://localhost:8888';
+
+	ttttt = toa.toaObjectsList;
+	for (var it in ttttt) {
+		ddd = "";
+		for (var jj in ttttt[it].bus) {
+			qqq = "<ul><li style='padding-left:25px;'>" + ttttt[it].bus[jj].bus + "<span style='color:" + ttttt[it].bus[jj].color + "; padding-left:15px;'>" + ttttt[it].bus[jj].time +" min</span></a></li></ul>";
+			ddd = ddd + qqq;
+		}
+		ccc = "<ul class='submenu'><li><div class='search-block'><div class='input-group'>"+ddd+"</div><!-- /input-group --></div><!-- /search-group --></li></ul>";
+		ppp = "<li class='openable'><a href='#'><span class='submenu-label'>" + ttttt[it].cod + " " + ttttt[it].name + "</span></a>"+ccc+"</li>";
+		$('#stoplightOpenable').append($(ppp))
+		
+	}
 
 	//Collapsible Sidebar Menu
 	$('.openable > a').click(function()	{
@@ -78,9 +92,9 @@ $(function	()	{
 
     $("#main-container").css("height",$(window).height() -45+"px");
 
-	
-	var item = [];
-
+	//autobus
+	item = [];
+	$("<li class='li-search li-search-message' style='display:none'><a href><span class='submenu-label'>No results</span></a></li>").appendTo($('#myline'))
 	 $.each(routes.routesObjectsList, function( key, datata ) {
 			var from;
 			var to;
@@ -109,55 +123,332 @@ $(function	()	{
 	 			from : from,
 				toward : to
 	 		});
-	 		$("<li><a href=''><span class='submenu-label'>" + datata.line + " " + from.name + "</span></a></li>").appendTo($('#myline'))
+	 		$("<li class='li-search route_" + datata.id + "' data-id=\"" + datata.id + "\"><a href=''><span class='submenu-label'>" + datata.line + " " + to.name + "</span></a></li>").appendTo($('#myline'))
 	  });
 
 
 
-	 $(function() {
+    $("#spotlight").on('keyup',function(e){
+    	/*if(!((e.which > 47 && e.which < 58) || (e.which >68 && e.which< 91))) {
+    		e.preventDefault();
+    		return
+    	}*/
+    	var search = $(this).val();
+    	if (search.length) {
+    		var arr = [];
+    		var i = 0;
+    		var str = "";
+    		var first = true;
+    		ss = search.toLowerCase().trim().split(' ');
 
-    var availableTags = [
-      "ActionScript",
-      "AppleScript",
-      "AppleScript2",
-      "AppleScript3",
-      "Asp",
-      "BASIC",
-      "C",
-      "C++",
-      "Clojure",
-      "COBOL",
-      "ColdF",
-      "Erlang",
-      "Fortran",
-      "Groovy",
-      "Haskell",
-      "Java",
-      "JavaScript",
-      "Lisp",
-      "Perl",
-      "PHP",
-      "Python",
-      "Ruby",
-      "Scala",
-      "Scheme"
-    ];
-        
-        
-    $("#spotlight").autocomplete({
-		source: availableTags,
-		appendTo: $("#myline")
-	});
+    		for (var it in item) {
+    			var ok = true;
+				for(var s in ss) {
+					if (ss[s].length) {
+						if ((item[it].name+" "+item[it].toward['name']).toLowerCase().indexOf(ss[s]) == -1){
+							ok = false;
+						}
+					}
+				}
+    			if (ok){
+    				arr[i] = "route_" + item[it].id;
+    				if (!first) {
+    					str += ","
+    				}
+    				str += " .route_" + item[it].id;
+    				first = false;
+    				i++;
+    			}
+    		}
+    		if (str.length) {
+    			$('.li-search-message').hide();
+    			$('.li-search').addClass('disable-route');
+    			$(str).removeClass('disable-route')
+    		} else {
+    			$('.li-search').addClass('disable-route');
+    			$('.li-search-message').show();
+    			console.log("Non ci sono elementi")
+    		}
+    		
+    	} else {
+    		$('.li-search-message').hide();
+    		$('.li-search.disable-route').removeClass('disable-route');
+    	}
+    });
 	
-	$("#spotlight").data( "ui-autocomplete" )._renderMenu = function( ul, items ) {
-		var that = this;		
-		ul.attr("class", "nav nav-pills nav-stacked");
-		$.each( items, function( index, item ) {
-			that._renderItemData( ul, item );
+//timepicker
+$('#timepicker').timepicker();
+
+//on blur from nominatim field the function fromto is called
+$('.nominatim').blur(fromto);
+
+
+
+$('#searchgo').click(function(){
+	if(nominatim1.value.length == 0){
+		$("#nominatim1").addClass("error");
+	}
+	else
+		$("#nominatim1").removeClass("error");
+
+	if(nominatim2.value.length == 0){
+		$("#nominatim2").addClass("error");
+	}
+	else
+		$("#nominatim2").removeClass("error");
+});
+
+$('.li-search').click(function(e){
+		e.preventDefault();
+		var aData = $(e.currentTarget).data("id");
+		$.getJSON( baseURL + "/routesearch?routeId="+ aData, function(data) {
+			showRoute(data);
 		});
-	};	    
-    
-});
+	});
+
+
+function dateToInt() {
+	var time = $("#timepicker").val();
+	var array = time.split(":");
+	var hours = parseFloat(array[0]);
+	var minutes = parseFloat(array[1]);
+	var dayVal;
+	switch ($("#stato").val()) {
+	case ("0"):
+		dayVal = 2880;
+		break;
+	case ("6"):
+		dayVal = 1440;
+		break;
+	default:
+		dayVal = 0;
+	}
+	if (hours === 12 && time.indexOf('AM') !== -1) {
+		hours = 0;
+	} else if (hours !== 12 && time.indexOf('PM') !== -1) {
+		hours += 12;
+	}
+	return (dayVal + (hours * 60 + minutes));
+}
+
+// setTimeout(
+//   function() 
+//   {$('#myline').slimScroll({
+//       alwaysVisible: true,
+//       railVisible: true,
+
+//       railColor: '#BECFE0'
+//   });
+//   }, 0);
+
+
+
+
+
+$(".photon-input").on('keyup',function(e){
+	var input = $(e.currentTarget)
+	var that = $(e.currentTarget).parent();
+	if (this.value.length > 4) {
+	$('.photon-autocomplete').empty();	
+	var komoot = 'http://photon.komoot.de/api/?q=';
+	// var city = 'l%27aquila';
+	var street = this.value.replace(" ","%20");
+	var lat = '42.3540';
+	var lon = '13.3919';
+	var result = 2;
+	var lang = 'it'; 
+	
+	$.getJSON(komoot + street + "&limit=" + result + "&lat="+ lat + "&lon=" + lon + "&lang=" + lang, function(data) {
+		$('.photon-autocomplete').hide();
+		$(".photon-autocomplete li.photon-row").remove();
+		if(data.features.length == 0){
+			that.addClass("photon-no-result");
+				$("<li class='photon-row'><strong class=''>" + "No results" + "</strong><small class=''></small></li>").appendTo($('ul.photon-autocomplete',that))
+
+			}
+		else {	
+
+		var uniquename = [];
+
+		for (var i in data.features) {
+			if (uniquename.indexOf(data.features[i].properties.name) == -1) {
+
+				uniquename.push(data.features[i].properties.name)
+				$("<li class='photon-row' id='" + data.features[i].properties.osm_id + "'><strong class='' data-name='" + data.features[i].properties.name + "'>" + data.features[i].properties.name + "</strong><small class=''>" + data.features[i].properties.city + "</small></li>")
+					.on('click', function(ev){
+						input.val($("strong", ev.currentTarget).attr('data-name'))
+					})
+					.appendTo($('ul.photon-autocomplete',that));
+			}
+			// console.log(data.features[i].properties.osm_id); 
+			// console.log(data.features[i].properties.name);
+			// console.log(data.features[i].properties.city);
+		}	
+		}	
+		$('ul.photon-autocomplete',that).show();
+	});
+    }
 });
 
 
+
+$('.nominatim').blur(function(){
+	setTimeout(function(){$('.photon-autocomplete').hide();}, 200)
+		
+});
+
+
+// $('#item1').blur(function () {
+//     var item1var = $('#item1').val();
+//     $('#item2').val(item1var);
+// });
+
+
+// $('#photon-autocomplete').click(function(e){
+// 		e.preventDefault();
+// 		var aData = $(e.currentTarget).data("name");
+// 		debugger;
+// 		console.log(aData);
+// 	});
+
+
+var results = [];//global scope
+var resultsInterval = -1;
+
+function latlon(e, niente) {
+	var URL = 'http://nominatim.openstreetmap.org/search?q=';
+   	var citta = ',+laquila';
+  	var format = 'json';
+  	var start = e.replace(" ","%20");
+	$.getJSON(URL + start + citta + "&format=" + format, function( nomi ) {
+		if (nomi[0]) {
+			results[niente] = nomi[0];
+		} else {
+			results[niente] = 'noresult';
+		}
+	});
+};
+
+
+$('#searchgo').click(function(e){
+	var time = dateToInt(timepicker);
+
+	clearInterval(resultsInterval);
+	results = []
+
+	var directions =$('#directionsPanel');
+	directions.empty();
+	var gggg = $("<div></div>");
+	gggg.css({
+		"height":"10px",
+		"width":"10px",
+		"textAlign":"center"
+	});
+	directions.append(gggg);
+	gggg.spin();
+  	latlon($("#nominatim1").val(), 0);
+  	latlon($("#nominatim2").val(), 1);
+  	
+  	resultsInterval = setInterval(function(){
+  		if (results[0] && results[1] ) {
+  			clearInterval(resultsInterval);
+  			if (results[0] == 'noresult' || results[1] == 'noresult') {
+  				var directions =$('#directionsPanel');
+				directions.hide();
+				directions.empty();
+  				$("#statusPanel").html("<p>No Path Found</p>")
+				$("#statusPanel").addClass("error");
+  			} else {
+  				var URL2 =  baseURL + "directions?lat1=";
+				item = [];
+				$("<li class='' style='display:none'><a href><span class='submenu-label'>No results</span></a></li>").appendTo($('#prova'))			
+				$.getJSON(URL2 + results[0].lat + "&lon1=" + results[0].lon + "&lat2="+ results[1].lat + "&lon2=" + results[1].lon + "&time=" + time, function(data) {
+					$('#directionsPanel .outstage').remove();
+					if (data == null) {
+						var directions =$('#directionsPanel');
+						directions.hide();
+						directions.empty();
+						$("#statusPanel").html("<p>No Path Found</p>")
+						$("#statusPanel").addClass("error");
+						console.log("No Path Found");
+					} else {
+						showDirections(data);
+					}
+				});
+  			}
+
+  			
+  		}
+  		
+  	},300);
+	
+
+	
+});
+
+
+function showDirections(json) {
+	var directions =$('#directionsPanel');
+	directions.hide();
+	directions.empty();
+    directions.prepend("<div id='about'><div id='about_in' class='acc_container_dir'><h4>directions</h4></div></div>");
+	$("#statusPanel").empty();
+	processWalks(json);
+	processRoutes(json);
+	directions.slideDown();
+
+}
+
+function processWalks(json) {
+	var walks = json.directionslist[0].walks;
+	
+	for (i = 0; i < walks.length; i++) {
+		console.log(walks[i]);
+		var deptStation = (i==walks.length-1) ?  "to Destination" : json.directionslist[0].routes[i].deptStation;
+		var stg = $('<div id="stage'+i+'" class="outstage"></div>');
+		var walk = $('<div id="walk'+i+'" class="stage walk">'+
+			'<span class="directionsDisplayIcon directionsWalkIcon">'+
+			'<small> Walk to ' + deptStation + '</small>'+
+			'</span>'+
+			'<p>' + walks[i].distance + ' mins</p>'+
+			'</div>');
+
+		stg.append(walk);
+
+		$('#directionsPanel').append(stg);
+	}
+
+	
+
+	}
+
+function processRoutes(json) {
+	var routes = json.directionslist[0].routes;
+	
+	for (i = 0; i < routes.length; i++) {
+
+		var routetemplate = '<div id="route'+i+'" class="stage bus"><span class="directionsDisplayIcon directionsBusIcon"> \
+			<small> Take the <%= routeId %>  Bus (<%= numOfStops %> Stops)</small> \
+			</span> <p><%= deptTime %> - <%= arrTime %></p> \
+			<p><i>Get off at: </i><%= arrStation %></p></div>';
+
+		routetemplate = routetemplate.replace('<%= routeId %>', routes[i].routeId);
+		routetemplate = routetemplate.replace('<%= numOfStops %>', routes[i].numOfStops);
+		routetemplate = routetemplate.replace('<%= deptTime %>', routes[i].deptTime);
+		routetemplate = routetemplate.replace('<%= arrTime %>', routes[i].arrTime);
+		routetemplate = routetemplate.replace('<%= arrStation %>', routes[i].arrStation);
+
+		var stg = $('div#stage'+i);
+
+		var route = $(routetemplate);
+
+		stg.append(route);
+
+	}
+
+}
+
+
+
+
+});
